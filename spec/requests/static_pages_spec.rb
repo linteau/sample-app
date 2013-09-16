0457c3654ce308ced611b34f5a3d97f9325ddb50
andylinteau <andy.linteau@buystand.com>
2013-09-16 16:14:57 +0800

    add contact page, dry up tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe "StaticPages" do
-  
+  let(:base_title) { "Ruby on Rails Tutorial Sample App"}
   describe "Home page" do
     
     it "should have the content 'Sample App'" do
@@ -11,7 +11,7 @@ describe "StaticPages" do
     
     it "should have the title 'home'" do
       visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+      expect(page).to have_title("#{base_title} | Home")
     end
   end
   
@@ -24,7 +24,7 @@ describe "StaticPages" do
     
     it "should have the title 'help'" do
       visit '/static_pages/help'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+      expect(page).to have_title("#{base_title} | Help")
     end
   end
   
@@ -37,7 +37,20 @@ describe "StaticPages" do
     
     it "should have the title 'about'" do
       visit '/static_pages/about'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+      expect(page).to have_title("#{base_title} | About Us")
+    end
+  end
+  
+  describe "Contact page" do
+    
+    it "should have content 'About Us'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact Us')
+    end
+    
+    it "should have title 'Contact Us'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("#{base_title} | Contact Us")
     end
   end
 end
