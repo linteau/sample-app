bb639a9da903ae4eee3094168dbfd0dac88591de
andylinteau <andy.linteau@buystand.com>
2013-09-24 21:46:23 +0800

    add functions to utilities for specs

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -7,8 +7,15 @@ def full_title(page_title)
   end
 end
 
-# def valid_signin(user)
-#   fill_in "Email",    with: user.email
-#   fill_in "Password", with: user.password
-#   click_button "Sign in"
-# end
+def valid_signin(user)
+  fill_in "Email",    with: user.email
+  fill_in "Password", with: user.password
+  click_button "Sign in"
+end
+
+def valid_signup
+  fill_in "Name", with: "Example User"
+  fill_in "Email", with: "user@example.com"
+  fill_in "Password", with: "foobar"
+  fill_in "Confirmation", with: "foobar"
+end
